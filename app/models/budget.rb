f7eb5facc0c5db522f9b67a1f9a2a09ec4de47db
eduardosimonpicon <eduardo.simon@flywire.com>
2023-03-12 13:05:30 +0100

    add uncategorized view

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,4 +2,9 @@ class Budget < ApplicationRecord
   validates :title, presence: true
   has_many :categories, dependent: :destroy
   has_many :accounts, dependent: :destroy
+  has_many :movements, through: :accounts
+
+  def uncategorized_movements
+    movements.where(category_id: nil)
+  end
 end
