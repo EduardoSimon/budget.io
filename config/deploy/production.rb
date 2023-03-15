2b3af4e839086f0804f2368a6fd1411dbe1fa7a7
eduardosimonpicon <eduardo.simon@flywire.com>
2023-03-15 19:47:37 +0100

    change deploy folder

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server "192.168.1.201", user: "rails", roles: %w[app db web], primary: "true"
-set :deploy_to, "/var/www/budgetio"
+set :deploy_to, "/home/rails/budgetio"
 set :branch, "main"
 set :stage, :production
 set :rails_env, :production
