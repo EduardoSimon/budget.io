10331dfd9015ce71a806757ba7a22fcf05d0927c
eduardosimonpicon <eduardo.simon@flywire.com>
2023-03-18 12:13:16 +0100

    feat: auto submit category change on budget view

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,7 +4,7 @@ class MovementsController < ApplicationController
   def update
     respond_to do |format|
       if @movement.update(movement_params)
-        format.html { redirect_to account_movement_url(@account, @movement), notice: "Account was successfully updated." }
+        format.html { redirect_to budget_url(@account.budget), notice: "Account was successfully updated." }
         format.json { render :show, status: :ok, location: @movement }
       else
         format.html { render :edit, status: :unprocessable_entity }
