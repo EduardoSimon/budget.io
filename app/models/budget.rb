1539a8f9747120a561da7a3da61706962aad4b04
eduardosimonpicon <eduardo.simon@flywire.com>
2023-04-10 13:29:46 +0200

    add ready to budget calculation test

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -7,4 +7,11 @@ class Budget < ApplicationRecord
   def uncategorized_movements
     movements.where(category_id: nil)
   end
+
+  def ready_to_budget
+    debits_without_category_sum = movements.debits.without_category.sum("amount_cents")
+    assigned_cents_in_categories = categories.sum("assigned_amount_cents")
+    ready_to_budget_cents = debits_without_category_sum - assigned_cents_in_categories
+    Money.new(ready_to_budget_cents, ready_to_budget_currency)
+  end
 end
--- test/models/budget_test.rb
+++ test/models/budget_test.rb
@@ -21,4 +21,19 @@ class BudgetTest < ActiveSupport::TestCase
 
     assert_equal(budget.accounts.first.id, account.id)
   end
+
+  test "ready to budget amount is the difference between the sum of debits without category and the sum of assigned cents in every category" do
+    movement_amount = 10000
+    category_assigned_amount = 5000
+
+    institution = institutions(:revolut)
+    budget = Budget.create(title: "test")
+    category = Category.create!(name: "test", budget_id: budget.id, assigned_amount_cents: category_assigned_amount)
+    account = Account.create!(budget: budget, institution: institution)
+    Movement.create!(account: account, category: category, amount_cents: movement_amount, payer: "with category")
+    Movement.create!(account: account, amount_cents: movement_amount, payer: "without category")
+    Movement.create!(account: account, amount_cents: movement_amount, payer: "without category")
+
+    assert_equal(budget.ready_to_budget, Money.new((movement_amount * 2) - category_assigned_amount, budget.ready_to_budget_currency))
+  end
 end
