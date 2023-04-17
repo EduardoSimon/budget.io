733eff99298e72e2059641fea84b6dda17aba89c
eduardosimonpicon <eduardo.simon@flywire.com>
2023-04-17 18:35:51 +0200

    fix budgets controller test

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -22,7 +22,6 @@ class BudgetsController < ApplicationController
   # POST /budgets or /budgets.json
   def create
     @budget = Budget.new(budget_params)
-    create_ready_to_assign_category!
 
     respond_to do |format|
       if @budget.save
@@ -60,10 +59,6 @@ class BudgetsController < ApplicationController
 
   private
 
-  def create_ready_to_assign_category!
-    ready_to_assign_category = Category.create(budget: @budget, name: "Ready to Assign")
-    @budget.update!(ready_to_assign_category_id: ready_to_assign_category.id)
-  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_budget
