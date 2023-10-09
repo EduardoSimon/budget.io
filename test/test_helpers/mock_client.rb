021b0091b9ed467c0e3854cb294bffae1085bee1
eduardosimonpicon <eduardo.simon@flywire.com>
2023-10-09 13:10:52 +0200

    test: OpenBankingConnector mock client with fake transactions

--- test/test_helpers/mock_client.rb
+++ test/test_helpers/mock_client.rb
@@ -1,9 +1,27 @@
-  class MockClient
-    def init_auth_session(institution_id:, internal_user_id:, redirect_url:)
-      OpenBankingConnector::AuthSession.new(url: "url1", id: "id")
-    end
-
-    def fetch_session_result(id)
-      OpenBankingConnector::AuthSession.new(url: "url1", id: "id", accounts: ["account1"])
-    end
+class MockClient
+  def init_auth_session(*)
+    OpenBankingConnector::AuthSession.new(url: "url1", id: "id")
   end
+
+  def fetch_session_result(*)
+    OpenBankingConnector::AuthSession.new(url: "url1", id: "id", accounts: ["account1"])
+  end
+
+  def fetch_movements(*)
+    transactions = []
+    2.times do |i|
+      transactions.push({
+        id: i,
+        date: Time.now.utc.iso8601,
+        amount: Money.new(10000 * i, "EUR"),
+        payer: "payer",
+        description: "description"
+      })
+    end
+    OpenBankingConnector::AccountResponse.new(
+      transactions: transactions,
+      balance: 500.00,
+      currency: "EUR"
+    )
+  end
+end
