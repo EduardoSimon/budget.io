4414e8711bb3d711150d6af390bd342d66b86d9a
eduardosimonpicon <eduardo.simon@flywire.com>
2023-12-03 12:36:43 +0100

    fix: invalid migration dependent key

--- db/migrate/20230108174719_add_budget_reference_to_category.rb
+++ db/migrate/20230108174719_add_budget_reference_to_category.rb
@@ -1,7 +1,7 @@
 class AddBudgetReferenceToCategory < ActiveRecord::Migration[7.0]
   def change
     change_table :categories do |t|
-      t.belongs_to :budget, index: true, foreign_key: true, dependent: :destroy
+      t.belongs_to :budget, index: true, foreign_key: true
     end
   end
 end
